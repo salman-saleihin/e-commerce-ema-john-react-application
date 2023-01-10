ff30e2f98761dff097ade0e9d3a24ca43fb4a5dd
salman-saleihin <saleihinkhan@gmail.com>
2023-01-10 17:06:20 +0600

    Product Component and sending dynamic data by props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
-import logo from '../src/images/logo.png' ;
+import Shop from './components/Shop/Shop';
+
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
