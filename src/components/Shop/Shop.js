0b1f7615d7086a9f8fdbb28a55884a87742aef82
salman-saleihin <saleihinkhan@gmail.com>
2023-02-18 00:34:22 +0600

    Refactor add to Cart, Shop's cart developed and problem solved

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -15,11 +15,31 @@ const Shop = () => {
 
     const handleAddProduct = (product) => {
         // console.log("Product Added" , product) ;
-        const newCart = [...cart, product];
-        setCart(newCart);
-        const sameProduct  = newCart.filter(pd => pd.key === product.key);
-        const count = sameProduct.length ;
-        addToDatabaseCart(product.key , count) ;
+
+        const toBeAddedKey = product.key ; 
+        const sameProduct  = cart.find(pd => pd.key === toBeAddedKey);
+
+        let count = 1;
+        let newCart ; 
+        if(sameProduct){
+            count = sameProduct.quantity + 1 ;
+            sameProduct.quantity =count ;
+            const others = cart.filter(pd => pd.key !== toBeAddedKey) ;
+            newCart = [...others, sameProduct] ;
+
+        } 
+        else {
+            product.quantity = 1 ;
+            newCart = [...cart , product] ;
+        }
+        
+        setCart(newCart);     
+        addToDatabaseCart(product.key , count ) ;
+
+        // const count = sameProduct.length ;
+
+        // const newCart = [...cart, product];
+
     }
 
     return (
@@ -36,5 +56,5 @@ const Shop = () => {
         </div>
     );
 };
-
+ 
 export default Shop;
